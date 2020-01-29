832b60c510c4e533f9dbe1a603b36797f8747774
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-29 01:21:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,25 +1,25 @@
 def using_push(Array, String)
-  
+  array.push(String)
 end
 
 def using_unshift(Array, String)
-  
+  Array.unshift(String)
 end
 
 def using_pop(Array)
-  
+  Array.pop
 end
 
 def pop_with_args(Array)
-  
+  Array.pop(2)
 end
 
 def using_shift(Array)
-  
+  Array.shift
 end
 
 def shift_with_args(Array)
-  
+  Array.shift(2)
 end
 
 def using_concat(Array, ArrayTwo)
